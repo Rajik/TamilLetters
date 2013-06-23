8742c394ecd5e1b298f89e2e985f991074d45336
Rajik <krajeswaribtech@gmail.com>
2013-06-23 21:17:52 +0530

    Initial commit

---
+++ src/Tamil.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Tamil extends JApplet  {
+    private JPanel panel1;
+    private JLabel TamilLabel;
+
+    public void init() {
+        String rt = "teriter\u0BA4\u0BAE\u0BBF\u0BB4\u0BCD";
+//        String rt="\u0B87\u0BA4\u0BC1"+" "+"\u0BA4\u0BAE\u0BB4\u0BB4\u0BCD";
+        Container cp= getContentPane();
+        cp.setLayout(new FlowLayout());
+        JLabel b=new JLabel();
+        b.setFont(new Font("Latha", Font.PLAIN,12));
+        b.setText(rt);
+        cp.add(b);
+    }
+
+
+}
